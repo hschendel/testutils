88c4adf9a34a33bb59028e2ec9ebf37cf1857ebc
hschendel <mail@hschendel.de>
2017-02-22 22:12:48 +0100

    Open file from testdata directory

---
+++ testutils.go
@@ -0,0 +1,25 @@
+package testutils
+
+import (
+	"path/filepath"
+	"os"
+	"testing"
+)
+
+// Opens a file from the package's testdata directory.
+// If that does not work, does a t.Fatalln() with the error and returns nil.
+func OpenTestDataFile(t *testing.T, fileName string) *os.File {
+	workingDir, getwdErr := os.Getwd()
+	if getwdErr != nil {
+		t.Fatal(getwdErr)
+		return nil
+	}
+	testFilePath := filepath.Join(workingDir, "testdata", fileName)
+	file, openErr := os.Open(testFilePath)
+	if openErr != nil {
+		t.Fatal(openErr)
+		return nil
+	} else {
+		return file
+	}
+}
---
+++ testutils_test.go
@@ -0,0 +1,22 @@
+package testutils
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestOpenTestDataFile(t *testing.T) {
+	file := OpenTestDataFile(t, "test.txt")
+	if file == nil {
+		return
+	}
+	content, readErr := ioutil.ReadAll(file)
+	if readErr != nil {
+		t.Fatal(readErr)
+		return
+	}
+	if len(content) < 4 || string(content[0:4]) != "Blah" {
+		t.Error("Expected \"Blah\", but found: ", string(content))
+	}
+}
+	
